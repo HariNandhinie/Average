094031a8b3f41cef43ca7b3658e623afbe5ec692
HariNandhinie <94113156+HariNandhinie@users.noreply.github.com>
2023-11-18 11:24:16 +0530

    Update Average.java

--- Average.java
+++ Average.java
@@ -1,17 +1,14 @@
-import java.util.Scanner;
-public class sum_avg {
+public class Average {
+
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter The Limit: ");
-        int n=in.nextInt();
-        int sum=0,a;
-        for(int i=1;i<=n;i++)
-        {
-            System.out.println("Enter The Number "+i+": ");
-            a=in.nextInt();
-            sum+=a;//sum=sum+a;
+        try {
+            int num1 = Integer.parseInt(args[0]);
+            int num2 = Integer.parseInt(args[1]);
+            double average = (num1 + num2) / 2.0;
+            System.out.println("The average of " + num1 + " and " + num2 + " is " + average);
+        } catch (Exception e) {
+            System.out.println("Error: Invalid input. Please provide two valid numbers.");
+            e.printStackTrace();
         }
-        System.out.println("The sum of given numbers is : "+sum);
-        System.out.println("The Average of given numbers is : "+sum/n);
     }
 }
