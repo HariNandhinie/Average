e7cc8adef2a6aea48cd0acd463ac6afbaa10bcd2
HariNandhinie <94113156+HariNandhinie@users.noreply.github.com>
2023-11-18 10:57:27 +0530

    Update Average.java

--- Average.java
+++ Average.java
@@ -1,13 +1,18 @@
-public class Average{
-   public static void main(String[] args){
-Scanner sc = new Scanner(System.in); 
-int sum = 1; 
-for (int i=1; i<=10; i++ ) { 
-    System.out.println("Enter number "); 
-    sum = sum + sc.nextInt(); 
-    int avg = sum/10; 
-}
-System.out.println("sum is "+ sum); 
-System.out.println("Avg is : "+ avg);
-   }
+import java.util.Scanner;
+//Write a program to find the sum and average of given n numbers.
+public class sum_avg {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter The Limit: ");
+        int n=in.nextInt();
+        int sum=0,a;
+        for(int i=1;i<=n;i++)
+        {
+            System.out.println("Enter The Number "+i+": ");
+            a=in.nextInt();
+            sum+=a;//sum=sum+a;
+        }
+        System.out.println("The sum of given numbers is : "+sum);
+        System.out.println("The Average of given numbers is : "+sum/n);
+    }
 }
